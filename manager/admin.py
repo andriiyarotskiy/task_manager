f09e91b06c8456348a79221f906f867929bde3a6
Andrii Yarotskiy <andriiyarotskiy@gmail,com>
2026-02-05 22:45:33 +0000

    Created app models

--- manager/admin.py
+++ manager/admin.py
@@ -1,3 +1,42 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth.models import Group
 
-# Register your models here.
+from manager.models import (
+    Position,
+    TaskType,
+    Tag,
+    Worker,
+    Team,
+    Project,
+    Task,
+)
+
+admin.site.register(Position)
+admin.site.register(TaskType)
+admin.site.register(Tag)
+admin.site.register(Team)
+
+
+@admin.register(Task)
+class TaskAdmin(admin.ModelAdmin):
+    list_display = (
+        "name",
+        "task_type",
+        "deadline",
+        "priority",
+        "completed",
+    )
+
+
+@admin.register(Project)
+class ProjectAdmin(admin.ModelAdmin):
+    list_display = ["name", "teams", "completed"]
+
+
+@admin.register(Worker)
+class WorkerAdmin(UserAdmin):
+    list_display = UserAdmin.list_display + ("position",)
+
+
+admin.site.unregister(Group)
--- manager/models.py
+++ manager/models.py
@@ -1,3 +1,109 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+from manager_service import settings
+
+
+class Position(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+
+    def __str__(self):
+        return self.name
+
+
+class TaskType(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Tag(models.Model):
+    name = models.CharField(
+        max_length=100,
+        unique=True,
+        blank=True,
+        null=True
+    )
+
+    def __str__(self):
+        return self.name
+
+
+class Worker(AbstractUser):
+    position = models.ForeignKey(
+        Position,
+        on_delete=models.CASCADE,
+        related_name="workers",
+        null=True,
+        blank=True,
+    )
+
+    class Meta:
+        verbose_name = "Worker"
+
+    def __str__(self):
+        return f"${self.username} - {self.first_name} {self.last_name}"
+
+
+class Team(models.Model):
+    name = models.CharField(max_length=100)
+    members = models.ForeignKey(
+        settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name="teams"
+    )
+
+    def __str__(self):
+        return self.name
+
+
+class Project(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+    completed = models.BooleanField(default=False)
+    teams = models.ForeignKey(
+        Team,
+        on_delete=models.CASCADE,
+        related_name="projects",
+        blank=True,
+        null=True
+    )
+
+    def __str__(self):
+        return self.name
+
+
+TASK_PRIORITIES = (
+    ("LOW", "Low"),
+    ("MEDIUM", "Medium"),
+    ("HIGH", "High"),
+    ("VERY HIGH", "Very High"),
+    ("URGENT", "Urgent"),
+)
+
+
+class Task(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField(max_length=500, blank=True, null=True)
+    deadline = models.DateTimeField(null=True, blank=True)
+    completed = models.BooleanField(default=False)
+    priority = models.TextField(
+        choices=TASK_PRIORITIES, default=TASK_PRIORITIES[0], blank=True, null=True
+    )
+
+    task_type = models.ForeignKey(
+        TaskType, on_delete=models.CASCADE, related_name="tasks"
+    )
+    assignees = models.ManyToManyField(
+        settings.AUTH_USER_MODEL,
+        blank=True,
+        related_name="tasks"
+    )
+    tags = models.ManyToManyField(Tag, related_name="tasks", blank=True)
+
+    project = models.ForeignKey(
+        Project,
+        on_delete=models.CASCADE,
+        related_name="tasks",
+    )
+
+    def __str__(self):
+        return f"{self.name} ({self.priority})"
