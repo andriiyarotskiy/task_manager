00ec366f9bea9a822596f043f66adb4cfc4b1006
Andrii Yarotskiy <andriiyarotskiy@gmail,com>
2026-02-10 18:08:24 +0000

    Connected postgresSQL

--- manager/models.py
+++ manager/models.py
@@ -2,7 +2,7 @@ from django.contrib.auth.models import AbstractUser
 from django.db import models
 from django.urls import reverse
 
-from manager_service import settings
+from django.conf import settings
 
 
 class Position(models.Model):
